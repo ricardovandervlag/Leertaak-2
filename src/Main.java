c76fe4372a7e20545778eb5c97092360f44c1571
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2017-12-07 21:18:10 +0100

    Changed public functions to private functions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 /* Main class for this application
- @author:   Ricardo van der Vlag
- @version:  2017-12-07
+ * @author:  Ricardo van der Vlag
+ * @version: 2017-12-07
  */
 public class Main {
     // Declares a string to be printed
     private String text;
     // Function to set the text
-    public void setText(String input){
+    private void setText(String input){
         text = input;
     }
     // Function to print the text
-    public void getText() {
+    private void getText() {
         System.out.println(text);
     }
-    // Main function to create an instance of Main.class
+    // Main function to create an instance of Main class
     // Function also sets text and prints it
     public static void main(String[] args) {
         Main application = new Main();
