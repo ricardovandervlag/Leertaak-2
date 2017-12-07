98fde63483dd54e9084f043f1827e675d3f629c5
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2017-12-07 22:34:22 +0100

    Added controller and buffer class

---
+++ src/Buffer.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+
+/*
+ * Class for buffering
+ * @author:  Ricardo van der Vlag
+ * @version: 2017-12-07
+ */
+public class Buffer {
+    //Constructor for making a buffer
+    private String buffer[];
+    //Empty buffer
+    private void emptyBuffer(){
+        int bufLen = buffer.length;
+        int i = 0;
+        while (i < bufLen){
+
+        }
+    }
+    //Fill buffer
+    private void fillBuffer(String add){
+        buffer = buffer +
+    }
+}
